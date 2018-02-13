4b3f058a5ff2b6eb570408f11426cc2b832b30d8
nazy <nazy2948@yahoo.ca>
2018-02-14 00:09:43 +0100

    add cityName

--- src/components/RestaurantList/RestaurantList.jsx
+++ src/components/RestaurantList/RestaurantList.jsx
@@ -1,17 +1,26 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 
+@connect(mapStateToProps)
 export class RestaurantList extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
 
-        const selectedCity = "Grenoble"
+        const { cityName } = this.props
 
         return (
         <div>
-            <h1>Welcome to {selectedCity}</h1>
+            <h1>Welcome to {cityName}</h1>
+            <h2>Hergit e is All Restaurants in {cityName}</h2>
         </div>
         )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        cityName: state.location.cityName
     }
 }
