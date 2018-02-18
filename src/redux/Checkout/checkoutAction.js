7cd8d0d6e1a87383e27356877ead5f4097a5489b
nazy <nazy2948@yahoo.ca>
2018-02-18 02:54:20 +0100

    add V2 sources to dev branch

--- src/redux/Checkout/checkoutAction.js
+++ src/redux/Checkout/checkoutAction.js
@@ -1,4 +1,4 @@
 import * as Actions from '../actions'
 import { createAction } from 'redux-actions';
 
-export const setTotalShoppingList = createAction(Actions.SET_TOTAL_SHOPPING_LIST, (items) => (items + 1)
+export const setTotalShoppingList = createAction(Actions.SET_TOTAL_SHOPPING_LIST, (items) => (items + 1))
